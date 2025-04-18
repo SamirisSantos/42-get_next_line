d2f98c0282df72748f4fe8b814bdbbaeb20d5811
Samiris Santos <samiris.araujo@gmail.com>
2025-04-18 14:59:02 +0100

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -13,7 +13,38 @@
 #include <stdio.h>
 #include <string.h>
 
-char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+char *ft_strnstr(const char *big, const char *little, size_t len)
 {
-    
+    size_t  i;
+    size_t  j;
+
+    i = 0;
+    j = 0;
+    if (little[i] == '\0')
+    {
+        return ((char *)&big[i]);
+    }
+    while ((big[i] != '\0') && (i < len - 1))
+    {
+        while (little[j] != '\0' && little[j] == big[i + j])
+        {
+            j++;
+        }
+        if (little[j] == '\0')
+        {
+            return ((char *)&big[i]);
+        }
+        i++;
+    }
+    return (0);
+}
+int main()
+{
+    const char *largestring = "Foo Bar Baz";
+    const char *smallstring = "Bar";
+    char *ptr;
+
+    ptr = ft_strnstr(largestring, smallstring, 4);
+    printf("Retorn: %s \n",ptr);
+
 }
