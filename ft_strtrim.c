8b76b8cdc5c862fb59b5a6c1558b680ad957d8ff
SamirisSantos <samiris.araujo@gmail.com>
2025-04-24 11:02:40 +0100

    completed function

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,32 +14,26 @@
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
-	size_t	j;
-	size_t	k;
-	char	*res;
+	size_t start;
+	size_t end;
+	char *strstrimed;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	if (set[i] == '\0')
-	{
-		return ((char *)&s1[i]);
-	}
-	while (s1[i] != '\0')
-	{
-		while (set[j] != '\0' && set[j] == s1[i + j])
-		{
-			j++;
-		}
-		printf(" %c \n",res[k]);
-		if(set[j] == '\0')
-		{
-			return ((char *)&s1[i]);
-		}
-		i++;
-	}
-	return (0);
+	if (!s1 || !set)
+		return (NULL);
+	
+	start = 0;
+	end = ft_strlen((char *)s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	strstrimed = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (!strstrimed)
+		return (NULL);
+	
+	ft_strlcpy(strstrimed, s1 + start, end - start + 1);
+	
+	return (strstrimed);
 }
 int main()
 {
