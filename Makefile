e9d2099a338ccca6bc0f31399583b9337934ae06
Samiris De Araujo Santos <sade-ara@c2r1s11.42porto.com>
2025-04-24 17:23:35 +0100

    add new function and update Makefile

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/04/15 14:28:22 by sade-ara          #+#    #+#              #
-#    Updated: 2025/04/24 14:32:52 by sade-ara         ###   ########.fr        #
+#    Updated: 2025/04/24 16:39:25 by sade-ara         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,7 @@ SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c\
 		ft_memmove.c ft_strlcpy.c ft_strlcat.c ft_tolower.c ft_toupper.c\
 		ft_strchr.c ft_strrchr.c ft_strncmp.c ft_memchr.c ft_memcmp.c\
 		ft_strnstr.c ft_atoi.c ft_calloc.c ft_strdup.c ft_substr.c\
-		ft_strjoin.c ft_strtrim.c
+		ft_strjoin.c ft_strtrim.c ft_itoa.c
 OBJS = $(SRCS:.c=.o)
 
 all: $(NAME)
--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,82 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 14:59:03 by sade-ara          #+#    #+#             */
-/*   Updated: 2025/04/24 15:04:00 by sade-ara         ###   ########.fr       */
+/*   Updated: 2025/04/24 17:22:25 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	countnumber(int i)
+{
+	int	count;
+
+	count = 0;
+	if (i < 0)
+	{
+		i = i * (-1);
+		count++;
+	}
+	while (i > 0)
+	{
+		i = i / 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_swapstr(char *c)
+{
+	char	temp;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (c[i] == '-')
+		i++;
+	j = (strlen(c) - 1);
+	while (j >= i)
+	{
+		temp = c[i];
+		c[i] = c[j];
+		c[j] = temp;
+		i++;
+		j--;
+	}
+	return (c);
+}
+
 char	*ft_itoa(int n)
 {
+	char	*s;
+	int		i;
 
+	s = (char *)malloc(sizeof(char *) * (countnumber(n) + 1));
+	if (!s)
+		return (NULL);
+	i = 0;
+	if (n == 0)
+		s[i] = '0';
+	if (n < 0)
+	{
+		s[i] = '-';
+		n = n * (-1);
+		i ++;
+	}
+	while (n > 0)
+	{
+		s[i] = n % 10 + '0';
+		n = n / 10;
+		i ++;
+	}
+	s[i] = '\0';
+	ft_swapstr(s);
+	return (s);
 }
+// int main()
+// {
+// 	int	n = -2025;
+// 	char	*converted_int;
+// 	converted_int = ft_itoa(n);
+// 	printf("int to string: %s\n", converted_int);
+// }
