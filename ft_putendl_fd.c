b1b6eaff127297174b50d950dace97d9362c36fe
SamirisSantos <samiris.araujo@gmail.com>
2025-04-27 21:44:20 +0100

    add new function, need test

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 16:36:23 by sade-ara          #+#    #+#             */
-/*   Updated: 2025/04/26 16:36:23 by sade-ara         ###   ########.fr       */
+/*   Updated: 2025/04/27 21:42:45 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 void ft_putendl_fd(char *s, int fd)
 {
-
+	if (s == NULL)
+		return;
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
+int main()
+{
+	char str[]= "Hello Piscine 42";
+	ft_putendl_fd(str, 1);
+	return (0);
+	
+	/*int fd = open("saida.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+	{
+		perror("Erro ao abrir o arquivo");
+		return 1;
+	}
+	
+	ft_putendl_fd(str, fd);
+	ft_putchar_fd('\n', fd);
+	close(fd);
+	return 0;*/
 }
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,11 +6,13 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 16:36:41 by sade-ara          #+#    #+#             */
-/*   Updated: 2025/04/27 17:23:47 by sade-ara         ###   ########.fr       */
+/*   Updated: 2025/04/27 21:38:04 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "libft.h"
+#include <fcntl.h>
 
 void ft_putnbr_fd(int n, int fd)
 {
@@ -20,7 +22,51 @@ void ft_putnbr_fd(int n, int fd)
 
 	i = 0;
 	nb = n;
+	
 	if(n == 0)
 		ft_putchar_fd('0', fd);
-	itoa(n, str, 10);
+	str = itoa(n);
+	if(!str)
+		return;
+	while(str[i])
+	{
+		ft_putchar_fd(str[i], fd);
+		i++;
+	}
+	free(str);
+}
+int main()
+{
+	int fd = 1; 
+	ft_putnbr_fd(-2147483648, fd); 
+	ft_putchar_fd('\n', fd);
+	ft_putnbr_fd(5432, fd);
+	ft_putchar_fd('\n', fd);
+	ft_putnbr_fd(0, fd);
+	ft_putchar_fd('\n', fd);
+	ft_putnbr_fd(-5432, fd);
+	ft_putchar_fd('\n', fd);
+	ft_putnbr_fd(2147483647, fd); 
+	return (0);
+	/*
+	// Testando a função com um arquivo
+	fd = open("saida.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1)
+    {
+        perror("Erro ao abrir o arquivo");
+        return 1;
+    }
+	ft_putnbr_fd(-2147483648, fd);
+	ft_putchar_fd('\n', fd);
+	ft_putnbr_fd(5432, fd);
+	ft_putchar_fd('\n', fd);
+	ft_putnbr_fd(0, fd);
+	ft_putchar_fd('\n', fd);
+	ft_putnbr_fd(-5432, fd);
+	ft_putchar_fd('\n', fd);
+	ft_putnbr_fd(2147483647, fd);
+	close(fd);
+	return 0;
+	// O conteúdo estara no arquivo "saida.txt"
+	*/
 }
