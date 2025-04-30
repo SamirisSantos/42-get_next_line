0e78fbc1b8ae54903f5fbf0fc8bcc5986dc235d5
SamirisSantos <samiris.araujo@gmail.com>
2025-04-30 19:53:26 +0100

    update

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 19:14:46 by sade-ara          #+#    #+#             */
+/*   Updated: 2025/04/30 19:14:46 by sade-ara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdint.h> // para uintptr_t
+
+int		ft_printf(const char *format, ...);
+
+int		ft_putchar(char c);
+
+#endif
