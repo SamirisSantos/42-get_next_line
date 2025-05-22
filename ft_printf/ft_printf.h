a17c4e865a9a1cd0cf2d86416eca5388438521f8
SamirisSantos <samiris.araujo@gmail.com>
2025-05-22 12:26:58 +0100

    update

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -20,12 +20,14 @@
 # include <stdio.h>
 # include <string.h>
 
+
 int	ft_printf(const char *format, ...);
-int	ft_putchar(char c)
+
+int	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_putnbr(int n);
 int	ft_putuint(unsigned int n);
 int	ft_puthex(unsigned int n, char format);
-int	ft_putpointer(void *ptr);
+int	ft_putptr(void *ptr);
 
 #endif
